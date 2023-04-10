ad4c992ffeb172b0ffbb68c07e2a097b693ac46a
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-04-10 18:05:50 +0700

    fully connected layer

--- numpyCNN/src/layers/fc.py
+++ numpyCNN/src/layers/fc.py
@@ -14,3 +14,26 @@ class FullyConnected(Layer):
         self.w = np.random.rand(self.in_dim, self.out_dim) * np.sqrt(2 / self.in_dim)
         self.b = np.zeros(1, self.out_dim)
 
+    def forward(self, input_tensor, training=True):
+        output_tensor = np.dot(input_tensor, self.w.T) + self.b
+        if training:
+            self.cache.update({'input_tensor': input_tensor, 'output_tensor':output_tensor})
+        return output_tensor
+
+    def backward(self, d_output):
+        input_tensor, output_tensor = (self.cache[key] for key in {'input_tensor', 'output_tensor'})
+        batch_size = input_tensor.shape[0]
+        dw = np.dot(d_output.T, input_tensor)
+        db = d_output.sum(axis=0, keepdims=True)
+        d_input = np.dot(d_output, self.w)
+        return d_input, dw, db
+
+    def update_params(self, dw, db, alpha=0.1):
+        self.w = self.w - alpha*dw
+        self.b = self.b - alpha*db
+
+    def get_params(self):
+        return self.w, self.b
+
+    def get_output_dim(self):
+        return self.out_dim
