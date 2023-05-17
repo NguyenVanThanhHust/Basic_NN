b1e86aa44b992cad72fc4d37e3f4266a4e27d08e
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-05-17 16:36:40 +0700

    forward convolution numpy

--- numpyCNN/src/layers/conv.py
+++ numpyCNN/src/layers/conv.py
@@ -2,65 +2,83 @@ import numpy as np
 from src.layers.layer import Layer
 
 class Conv(Layer):
-    def __init__(self, in_channel, out_channel, kernel_size, stride=0, padding_type='none', name='conv') -> None:
+    def __init__(self, in_channel, \
+            out_channel, \
+            kernel_size, \
+            stride=(1, 1), \
+            padding=(0, 0, 0, 0), \
+            name='conv', \
+            kernel_weight=None, \
+            ) -> None:
         self.in_channel = in_channel
         self.out_channel = out_channel
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding_type = padding_type
+        self.kernel_size = kernel_size if type(kernel_size) is tuple else (kernel_size, kernel_size) 
+        self.stride = stride if type(stride) is tuple else (stride, stride)
+        self.padding = padding
         self.kernel = None
         self.bias = None
         self.cache = {}
+        self.kernel = kernel_weight
+        self.name = name
         self.init()
 
     def init(self):
-        self.kernel = np.random.rand((self.out_channel, self.kernel_size[0], self.kernel_size[1], self.in_channel))
-        self.bias = np.zeros((1, 1, 1, self.out_channel))
-
-        output_h = int()
-
-    def forward(self, input_tensor, training):
-        batch_size = input.shape[0]
-        input_tensor = self.padding(input_tensor, self.padding_type)
+        if self.kernel is None:
+            print("Initialize randomly")
+            self.kernel = np.random.rand(self.out_channel, self.in_channel, self.kernel_size[0], self.kernel_size[1])
+        else:
+            print("Initialize from weight")
+            assert self.kernel.shape[0] == self.out_channel
+            assert self.kernel.shape[1] == self.in_channel
+            assert self.kernel.shape[2] == self.kernel_size[0]
+            assert self.kernel.shape[3] == self.kernel_size[1]
+            
+    def forward(self, input_tensor, training=True):
+        batch_size = input_tensor.shape[0]
+        input_tensor = self.pad(input_tensor)
         # Convolution
 
-        batch_size, input_h, input_w, input_c = input_tensor.shape
-        if self.padding_type == 'none':
-            output_h = int((input_h - self.kernel_size[0])/self.stride + 1)
-            output_w = int((input_w - self.kernel_size[1])/self.stride + 1)
-        else:
-            output_h = int((input_h - self.kernel_size[0] + 2)/self.stride + 1)
-            output_w = int((input_w - self.kernel_size[1] + 2)/self.stride + 1)
+        batch_size, input_c, input_h, input_w = input_tensor.shape
+        output_h = int((input_h - self.kernel_size[0])/self.stride[0]) + 1
+        output_w = int((input_w - self.kernel_size[1])/self.stride[1]) + 1
 
-        output_tensor = np.zeros((batch_size, self.out_channel, output_h, output_w))
-        for image_index in batch_size:
+        output_tensor = np.zeros((batch_size, self.out_channel, input_c, output_h, output_w))
+        for image_index in range(batch_size):
             input_image = input_tensor[image_index, :, :, :]
-            for channel in self.out_channel:
+            for channel in range(self.out_channel):
                 current_kernel = self.kernel[channel, :, :, :]
                 for h_index in range(output_h):
-                    v_start = h_index * self.stride
-                    v_end = v_start + self.stride
+                    h_start = h_index * self.stride[0]
+                    h_end = h_start + self.kernel_size[0]
+                    for w_index in range(output_w):
+                        w_start = w_index * self.stride[1]
+                        w_end = w_start + self.kernel_size[1]
+                        input_patch = input_tensor[image_index, :, h_start: h_end, w_start: w_end]
+                        input_patch = input_patch[np.newaxis, np.newaxis, ...]
+                        kernel = current_kernel[np.newaxis, np.newaxis, ...]
+                        output_patch = input_patch * kernel
+                        output_patch = output_patch.sum(axis=4)
+                        output_patch = output_patch.sum(axis=3)
+                        output_tensor[image_index, channel, :, h_index, w_index] = output_patch                             
 
-        return super().forward(input, training)
+        return output_tensor
     
     def backward(self, da):
         return super().backward(da)
     
     def get_params(self):
-        return self.kernel, self.bias
+        return self.kernel
 
     def update_params(self):
         return 
             
-    def get_output_dim(self):
-        return self.out_channel
+    def pad(self, x):
+        top, bottom, left, right = self.padding
+        x = np.pad(x, (top, bottom), mode='constant', constant_values=0)
+        x = np.pad(x, (left, right), mode='constant', constant_values=0)
+        return x
     
-    def padding(self, x, padding_type='none'):
-        assert padding_type in ['none', 'zero']
-        if padding_type == 'none':
-            return x
-        if padding_type == 'zero':
-            batch_size, h, w, c = x.shape
-            tmp_x = np.zeros(batch_size, h+2, w+2, c)
-            tmp_x[:, 1:-1, 1:-1, :] = x
-            return tmp_x
+    def __repr__(self) -> str:
+        conv_name = self.name
+        conv_name = conv_name + " layer with kernel shape: ({}, {}, {})".format(self.out_channel, self.in_channel, self.kernel_size)
+        return conv_name
---
+++ numpyCNN/test/test_layers/test_conv.py
@@ -0,0 +1,51 @@
+import os, sys
+import torch
+import torch.nn as nn
+import numpy as np
+import math 
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_blobs
+
+from loguru import logger
+
+sys.path.append("..")
+sys.path.append("../..")
+from src.layers.conv import Conv
+
+def test_convolution_forward():
+    # How to assign kernel for Conv layer
+    # https://discuss.pytorch.org/t/setting-custom-kernel-for-cnn-in-pytorch/27176/4
+    batch_size = 2
+    input_c, input_h, input_w = 3, 16, 24
+    input_array = np.random.rand(batch_size, input_c, input_h, input_w)
+    
+    input_torch = torch.from_numpy(input_array)
+    
+    kernel_numpy = np.random.rand(8, 3, 4, 3)
+    kernel_numpy = np.round(kernel_numpy, 4)
+    kernel_torch = torch.from_numpy(kernel_numpy)
+    
+    conv = nn.Conv2d(8, 3, (4, 3), bias=False)
+    with torch.no_grad():
+        conv.weight = nn.Parameter(kernel_torch)
+        
+    output_torch = conv(input_torch)
+    numpy_conv = Conv(3, 8, (4, 3), kernel_weight=kernel_numpy)
+    output_numpy = numpy_conv.forward(input_array)
+    output_numpy = output_numpy.sum(axis=2)
+    # output_numpy = np.round(output_numpy, 4)
+    
+    output_torch = output_torch.detach().cpu().numpy()
+    np.testing.assert_almost_equal(output_torch, output_numpy)
+
+
+def main():
+    np.random.seed(42)
+    test_convolution_forward()
+    return 
+
+if __name__ == "__main__":
+    main()
