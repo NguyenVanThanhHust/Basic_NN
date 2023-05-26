ea9f5ded3adcd7882d4b30f6a0f19ff4ca5e6763
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-05-26 09:07:33 +0700

    mse loss

--- numpyCNN/src/cost.py
+++ numpyCNN/src/cost.py
@@ -9,13 +9,24 @@ class CostFunction:
     def grad(self, pred, gt):
         raise NotImplementedError
     
-class CrossEntropyLoss(CostFunction):
+class MSELoss(CostFunction):
+    def __init__(self, reduction_method="mean"):
+        self.cache = {}
+        self.reduction_method = reduction_method
+        
     def forward(self, pred, gt):
         batch_size = gt.shape[0]
-        cost = 1/ batch_size * (pred - gt)**2
-        return cost
+        cost = (pred - gt)**2
+        self.cache["pred"] = pred
+        self.cache["gt"] = gt
+        if self.reduction_method == "mean":
+            return cost.mean()
+        else:
+            return cost.sum()
     
-    def backward(self, pred, gt):
+    def backward(self,):
+        gt = self.cache["gt"]
+        pred = self.cache["pred"]
         batch_size = gt.shape[0]
         cost = 1/ batch_size * (pred - gt)
         return cost
