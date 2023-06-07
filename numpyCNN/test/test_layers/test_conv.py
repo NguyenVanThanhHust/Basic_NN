e553c2b3fe7fd23a1b2d63053e3eb4922f3e145c
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-06-07 23:38:11 +0700

    add test, failing

--- numpyCNN/test/test_layers/test_conv.py
+++ numpyCNN/test/test_layers/test_conv.py
@@ -14,8 +14,63 @@ sys.path.append("../..")
 from src.layers.conv import Conv
 from src.cost import MSELoss, FakeL1Loss
 
-def test_convolution():
+def test_convolution_v2():
+    # How to assign kernel for Conv layer
+    # https://discuss.pytorch.org/t/setting-custom-kernel-for-cnn-in-pytorch/27176/4
 
+    # forward numpy
+    batch_size = 2
+    input_c, input_h, input_w = 3, 16, 24
+    input_array = np.random.rand(batch_size, input_c, input_h, input_w)
+    out_channel, in_channel, k_y, k_x = 8, 3, 4, 3
+    kernel_numpy = np.random.rand(out_channel, in_channel, k_y, k_x)
+    kernel_numpy = np.round(kernel_numpy, 4)
+    conv_numpy = Conv(3, 8, (4, 3), kernel_weight=kernel_numpy, reduction_method="mean")
+    output_numpy = conv_numpy.forward(input_array)
+    output_numpy = output_numpy.sum(axis=2)
+    
+    # forward torch
+    input_torch = torch.from_numpy(input_array)
+    kernel_torch = torch.from_numpy(kernel_numpy)
+    conv = nn.Conv2d(8, 3, (4, 3), bias=False)
+    input_torch.requires_grad = True
+    with torch.no_grad():
+        conv.weight = nn.Parameter(kernel_torch)
+    output_torch = conv(input_torch)
+    output_torch.retain_grad()
+
+    # backward numpy
+    target_numpy = np.zeros(output_numpy.shape, dtype=np.float32)
+    mse_loss_numpy = MSELoss()
+    loss_numpy = mse_loss_numpy.forward(output_numpy, target_numpy)
+    d_output = mse_loss_numpy.backward()
+    d_output = d_output / max(input_array.shape)
+    d_weight, d_input = conv_numpy.backward(d_output)
+
+    # backward torch
+    target_torch = torch.from_numpy(target_numpy)
+    target_torch = target_torch.double()
+    l2_loss =  nn.MSELoss()
+    loss_value_torch = l2_loss(output_torch, target_torch)
+    loss_value_torch.backward()
+
+
+    output_torch_np = output_torch.detach().cpu().numpy()
+    np.testing.assert_almost_equal(output_torch_np, output_numpy)
+    print("Pass forward test")
+
+    np.testing.assert_almost_equal(output_torch.grad.detach().cpu().numpy()*1144, d_output, decimal=5)
+    print("Pass backwared test for derivative of output")
+
+    np.testing.assert_almost_equal(conv.weight.grad.detach().cpu().numpy(), d_weight, decimal=5)
+    print("Pass backwared test for derivative of weight")
+
+    np.testing.assert_almost_equal(input_torch.grad.detach().cpu().numpy(), d_input, decimal=5)
+    print("Pass backwared test for derivative of input tensor")
+
+
+
+def test_convolution():
     # forward numpy
     batch_size = 1
     in_channel, input_h, input_w = 1, 6, 5
@@ -193,9 +248,10 @@ def test_convolution_manual():
 
 def main():
     np.random.seed(42)
-    test_convolution_manual()
-    test_mse_loss()
-    test_convolution()
+    # test_convolution_manual()
+    # test_mse_loss()
+    # test_convolution()
+    test_convolution_v2()
 
 if __name__ == "__main__":
     main()
